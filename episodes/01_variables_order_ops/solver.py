4bce5072da37f4a4e20047b4701d3f23b11cf460
Cesar V <cesarvillarreal11@gmail.com>
2025-05-22 10:55:05 -0400

    Scaffold repo: env files, README, MIT license, episode folders

---
+++ episodes/01_variables_order_ops/solver.py
@@ -0,0 +1,5 @@
+"""Episode 01 helper functions: variables & order of operations."""
+
+def roi(gain: float, cost: float) -> float:
+    """Return return-on-investment as a decimal."""
+    return (gain - cost) / cost
---
+++ episodes/02_linear_functions/line_tools.py
@@ -0,0 +1,5 @@
+"""Episode 02 helper functions: linear functions & graphs."""
+
+def y(mx: float, b: float, x: float) -> float:
+    """Compute y = m x + b."""
+    return mx * x + b
